3c11c0026d3192582f81ba68f756c71037270c26
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-03-21 14:13:12 +0000

    finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,43 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+public static void selectionSortName(String[] names, int[] ages){
+	int tempInt;
+	String tempStr;
+	int min = 0;
+	for(int i = 0; i < names.length - 1; i++){
+		min = i;
+for (int j = i + 1; j < names.length; j++){
+	if(((names[j].toLowerCase()).compareTo(names[min].toLowerCase())) < 0){
+min = j;
+	}
+}
+tempInt = ages[i];
+ages[i] = ages[min];
+ages[min] = tempInt;
+tempStr = names[i];
+names[i] = names[min];
+names[min] = tempStr;
+	} 
+}
 	
-
+public static void selectionSortAge(String[] names, int[] ages){
+	int tempInt;
+	String tempStr;
+	int min = 0;
+	for(int i = 0; i < names.length -1; i++){
+		min = i;
+for (int j = i + 1; j < names.length; j++){
+	if(ages[j] < ages[min]){
+min = j;
+	}
+}
+tempInt = ages[i];
+ages[i] = ages[min];
+ages[min] = tempInt;
+tempStr = names[i];
+names[i] = names[min];
+names[min] = tempStr;
+	} 
+}
 }
